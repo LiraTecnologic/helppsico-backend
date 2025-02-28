725068bbc7af9c9cdb5ffdb2fad5433fa7d1ecb7
Gustavo Nakamura <142915711+GustavoNakamuraC@users.noreply.github.com>
2025-02-28 18:06:41 -0300

    #BTR62:feat/Corrigindo nome da classe principal

--- src/main/java/com/liratech/helppsico/HelpPsicoApplication.java
+++ src/main/java/com/liratech/helppsico/HelpPsicoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelppsicoApplication {
+public class HelpPsicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelppsicoApplication.class, args);
