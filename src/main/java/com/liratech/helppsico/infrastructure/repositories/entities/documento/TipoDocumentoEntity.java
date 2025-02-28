b0e2d0c9157e3388ba293ff546ae6dbc7a8b4b4d
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-02-28 18:57:04 -0300

    #BTR04:feat/Adicionando a entidade SolicitacaoDocumento e a entidade pai Documento

---
+++ src/main/java/com/liratech/helppsico/infrastructure/repositories/entities/documento/TipoDocumentoEntity.java
@@ -0,0 +1,10 @@
+package com.liratech.helppsico.infrastructure.repositories.entities.documento;
+
+public enum TipoDocumentoEntity {
+    ATESTADO,
+    DECLARACAO,
+    RELATORIO_PSICOLOGICO,
+    RELATORIO_MULTIPROFISSIONAL,
+    LAUDO_PSICOLOGICO,
+    PARECER_PSICOLOGICO
+}
