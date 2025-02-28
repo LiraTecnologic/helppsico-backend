3cba4f0659b31d3f21adb876e2c2f098f7cb7f23
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-02-28 18:51:22 -0300

    #BTR04:feat/Adicionando entidades Avaliacao, HorarioPsicologo e Prontuario

---
+++ src/main/java/com/liratech/helppsico/infrastructure/repositories/entities/ProntuarioEntity.java
@@ -0,0 +1,33 @@
+package com.liratech.helppsico.infrastructure.repositories.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity(name = "Prontuario")
+@Table(name = "prontuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProntuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_prontuario")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_psicologo")
+    private PsicologoEntity psicologo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_paciente")
+    private PacienteEntity paciente;
+
+    private String titulo;
+    private String conteudo;
+
+
+}
