4dc5f019844448d549475223b449b25b8d6e1d40
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-06 15:37:41 -0300

    #BTR06:fix/Arrumando herança nos DTOs de Documento

--- src/main/java/com/liratech/helppsico/entrypoint/dto/documento/DeclaracaoDto.java
+++ src/main/java/com/liratech/helppsico/entrypoint/dto/documento/DeclaracaoDto.java
@@ -1,14 +1,27 @@
 package com.liratech.helppsico.entrypoint.dto.documento;
 
+import com.liratech.helppsico.entrypoint.dto.PacienteDto;
+import com.liratech.helppsico.entrypoint.dto.psicologo.PsicologoDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.UUID;
 
+@NoArgsConstructor
 @Getter
+@Setter
 public class DeclaracaoDto extends DocumentoDto{
     private String motivo;
     private String descricao;
     private String finalidade;
 
+    public DeclaracaoDto (UUID id, PacienteDto paciente, PsicologoDto psicologo, LocalDate dataEmissao, LocalDate dataValidade, String assinaturaPsicologo, String motivo, String descricao, String finalidade){
+        super(id, paciente, psicologo, dataEmissao, dataValidade, assinaturaPsicologo);
+        this.motivo = motivo;
+        this.descricao = descricao;
+        this.finalidade = finalidade;
+    }
 }
--- src/main/java/com/liratech/helppsico/entrypoint/dto/documento/LaudoPsicologicoDto.java
+++ src/main/java/com/liratech/helppsico/entrypoint/dto/documento/LaudoPsicologicoDto.java
@@ -1,12 +1,20 @@
 package com.liratech.helppsico.entrypoint.dto.documento;
 
+import com.liratech.helppsico.entrypoint.dto.EnderecoDto;
+import com.liratech.helppsico.entrypoint.dto.PacienteDto;
+import com.liratech.helppsico.entrypoint.dto.psicologo.PsicologoDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@AllArgsConstructor
+import java.time.LocalDate;
+import java.util.UUID;
+
+
 @NoArgsConstructor
 @Getter
+@Setter
 public class LaudoPsicologicoDto extends DocumentoDto{
     private String solicitante;
     private String objetivo;
@@ -17,4 +25,17 @@ public class LaudoPsicologicoDto extends DocumentoDto{
     private String respostaDemanda;
     private String recomendacoes;
     private String sigilo;
+
+    public LaudoPsicologicoDto (UUID id, PacienteDto paciente, PsicologoDto psicologo, LocalDate dataEmissao, LocalDate dataValidade, String assinaturaPsicologo, String solicitante, String objetivo, String historico, String procedimentosUtilizados, String descricaoResultados, String conclusao, String respostaDemanda, String recomendacoes, String sigilo){
+        super(id, paciente, psicologo, dataEmissao, dataValidade, assinaturaPsicologo);
+        this.solicitante = solicitante;
+        this.objetivo = objetivo;
+        this.historico = historico;
+        this.procedimentosUtilizados = procedimentosUtilizados;
+        this.descricaoResultados = descricaoResultados;
+        this.conclusao = conclusao;
+        this.respostaDemanda = respostaDemanda;
+        this.recomendacoes = recomendacoes;
+        this.sigilo = sigilo;
+    }
 }
