218634455ac13726208d3967fd43f59d11e00f45
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-07 18:20:58 -0300

    #BTR06:feat/Adicionando a validação para os Dto sem package

--- src/main/java/com/liratech/helppsico/entrypoint/dto/EnderecoDto.java
+++ src/main/java/com/liratech/helppsico/entrypoint/dto/EnderecoDto.java
@@ -1,6 +1,10 @@
 package com.liratech.helppsico.entrypoint.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 import java.util.UUID;
 
@@ -13,18 +17,25 @@ public class EnderecoDto{
     @JsonProperty("id")
     private UUID id;
 
+    @NotBlank(message = "A rua é obrigatória")
     @JsonProperty("rua")
     private String rua;
 
+    @NotNull(message = "O número é obrigatório")
+    @PositiveOrZero(message = "O número precisa ser maior ou igual a zero")
     @JsonProperty("numero")
     private Integer numero;
 
+
+    @NotBlank(message = "O cep é obrigatório")
     @JsonProperty("cep")
     private String cep;
 
+    @NotBlank(message = "A cidade é obrigatória")
     @JsonProperty("cidade")
     private String cidade;
 
+    @NotBlank(message = "O estado é obrigatório")
     @JsonProperty("estado")
     private String estado;
 }
--- src/main/java/com/liratech/helppsico/entrypoint/dto/PacienteDto.java
+++ src/main/java/com/liratech/helppsico/entrypoint/dto/PacienteDto.java
@@ -1,6 +1,12 @@
 package com.liratech.helppsico.entrypoint.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
@@ -15,33 +21,46 @@ public class PacienteDto {
     @JsonProperty("id")
     private UUID id;
 
+    @NotBlank(message = "O nome é obrigatório")
     @JsonProperty("nome")
     private String nome;
 
+    @NotBlank(message = "O cpf é obrigatório")
     @JsonProperty("cpf")
     private String cpf;
 
+    @NotBlank(message = "O email é obrigatório")
     @JsonProperty("email")
     private String email;
 
+    @NotBlank(message = "O email é obrigatório")
     @JsonProperty("telefone")
     private String telefone;
 
+    @Past
     @JsonProperty("dataNascimento")
     private LocalDate dataNascimento;
 
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&]).+$",
+            message = "A senha deve conter pelo menos uma letra, um número e um caractere especial.")
     @JsonProperty("senha")
     private String senha;
 
+    @NotBlank(message = "O gênero é obrigatório")
     @JsonProperty("genero")
+    @Enumerated(EnumType.STRING)
     private TipoGeneroDto genero;
 
     @JsonProperty("endereco")
     private EnderecoDto endereco;
 
+    @NotBlank(message = "A url da foto é obrigatória")
     @JsonProperty("fotoUrl")
     private String fotoUrl;
 
+    @NotBlank(message = "A foto é obrigatória")
     @JsonProperty("foto")
     private MultipartFile foto;
 }
