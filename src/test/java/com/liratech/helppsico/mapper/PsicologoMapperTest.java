a6bbf1d6bb88dc40fc44c7a72ad555fa4c2d713f
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-15 10:09:57 -0300

    #BTR07:test/Criando estruturas de Teste e realizando o teste de Avaliacao

---
+++ src/test/java/com/liratech/helppsico/mapper/PsicologoMapperTest.java
@@ -0,0 +1,20 @@
+package com.liratech.helppsico.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PsicologoMapperTest {
+
+    @Test
+    void paraDomain() {
+    }
+
+    @Test
+    void paraDto() {
+    }
+
+    @Test
+    void paraDtos() {
+    }
+}
