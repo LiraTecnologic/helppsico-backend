97a7f0c1220122c73f2c3a79635d56ceda6a25a0
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-17 17:15:35 -0300

    #BTR08:refactor/Alterando local dos mappers

--- src/main/java/com/liratech/helppsico/infrastructure/mappers/PacienteMapper.java
+++ src/main/java/com/liratech/helppsico/infrastructure/mappers/PacienteMapper.java
@@ -1,4 +1,4 @@
-package com.liratech.helppsico.mappers;
+package com.liratech.helppsico.infrastructure.mappers;
 
 import com.liratech.helppsico.domain.Paciente;
 import com.liratech.helppsico.infrastructure.repositories.entities.PacienteEntity;
--- src/main/java/com/liratech/helppsico/infrastructure/mappers/ProntuarioMapper.java
+++ src/main/java/com/liratech/helppsico/infrastructure/mappers/ProntuarioMapper.java
@@ -1,4 +1,4 @@
-package com.liratech.helppsico.mappers;
+package com.liratech.helppsico.infrastructure.mappers;
 
 import com.liratech.helppsico.domain.Prontuario;
 import com.liratech.helppsico.infrastructure.repositories.entities.ProntuarioEntity;
