438d0ee423e290965fd3ce1c39c3bf1171c72147
Vitor Hugo Vieira de Lima <vitorhvvieira@gmail.com>
2025-03-18 20:31:35 -0300

    #BTR17:test/Começando a desenvolver os builder para fazer os testes

---
+++ src/test/java/com/liratech/helppsico/builders/PsicologoBuilder.java
@@ -0,0 +1,27 @@
+package com.liratech.helppsico.builders;
+
+import com.liratech.helppsico.domain.Psicologo;
+import com.liratech.helppsico.domain.TipoGenero;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class PsicologoBuilder {
+    
+    public Psicologo gerarPsicologo() {
+        return Psicologo.builder()
+                .id(UUID.randomUUID())
+                .nome("Psicologo teste")
+                .crp("0100000")
+                .cpf("12332114763")
+                .email("emailteste@gmail.com")
+                .telefone("44987415623")
+                .dataNascimento(LocalDate.now())
+                .senha("senhateste123!")
+                .genero(TipoGenero.MASCULINO)
+                .enderecoAtendimento(EnderecoBuilder.gerarEndereco())
+                .fotoUrl("urltestefoto")
+                .biografia("Biografia teste")
+                .build();
+    }
+}
