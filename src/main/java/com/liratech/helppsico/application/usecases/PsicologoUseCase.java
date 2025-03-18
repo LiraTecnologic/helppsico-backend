92ab099ee31f33c35e4958f83cd48f26ec5b6b71
Vitor Hugo Vieira de Lima <vitorhvvieira@gmail.com>
2025-03-18 20:30:47 -0300

    #BTR17:feat/Finalizando o que podia ser codificado no PsicologoUseCase

--- src/main/java/com/liratech/helppsico/application/usecases/PsicologoUseCase.java
+++ src/main/java/com/liratech/helppsico/application/usecases/PsicologoUseCase.java
@@ -5,8 +5,10 @@ import com.liratech.helppsico.application.exceptions.PsicologoExistenteException
 import com.liratech.helppsico.application.exceptions.PsicologoNaoEncontradoException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,7 +23,7 @@ public class PsicologoUseCase {
     private final FotoUseCase fotoUseCase;
 
     public Psicologo cadastrar(Psicologo novoPsicologo) {
-        log.info("Cadastrar psicólogo. Novo psicólogo: {}", novoPsicologo);
+        log.info("Cadastrando psicólogo. Novo psicólogo: {}", novoPsicologo);
 
         Optional<Psicologo> psicologoExistente = gateway.consultarPorCrp(novoPsicologo.getCrp());
         psicologoExistente.ifPresent(psicologo -> {throw new PsicologoExistenteException();});
@@ -48,7 +50,7 @@ public class PsicologoUseCase {
     }
 
     public Psicologo consultarPorId(UUID id) {
-        log.info("Consultar psicólogo pelo sei id. Id: {}", id);
+        log.info("Consultando psicólogo pelo sei id. Id: {}", id);
 
         Optional<Psicologo> psicologo = gateway.consultarPorId(id);
 
@@ -61,6 +63,82 @@ public class PsicologoUseCase {
         log.info("Psicólogo consultado com sucesso. Psicólogo consultado: {}", psicologoEncontrado);
 
         return psicologoEncontrado;
+    }
+
+    public Psicologo consultarPorNome(String nome) {
+
+        log.info("Consultando psicólogo pelo nome. Nome a ser buscado: {}", nome);
+
+        Optional<Psicologo> psicologo = gateway.consultarPorNome(nome);
+
+        if(psicologo.isEmpty()) {
+            throw new PsicologoNaoEncontradoException();
+        }
+
+        Psicologo psicologoEncontrado = psicologo.get();
+
+        log.info("Psicólogo consultado com sucesso. Psicólogo: {}", psicologo);
+
+        return psicologoEncontrado;
+    }
+
+    public Page<Psicologo> consultarMelhoresAvaliados(Pageable pageable) {
+
+        log.info("Consultando psicólogos melhores avaliados.");
+
+        Page<Psicologo> psicologos = gateway.consultarMelhoresAvaliados(pageable);
+
+        log.info("Consultado psicólogos melhores avaliados. Psicólogos: {}", psicologos);
+
+        return psicologos;
+    }
+
+    public Psicologo consultarPorCrp(String crp) {
+
+        log.info("Consultando psicólogo pelo seu crp. Crp: {}", crp);
+
+        Optional<Psicologo> psicologo = gateway.consultarPorCrp(crp);
+
+        if(psicologo.isEmpty()) {
+            throw new PsicologoNaoEncontradoException();
+        }
+
+        Psicologo psicologoExistente = psicologo.get();
+
+        log.info("Consultado psicólogo pelo crp. Psicólogo: {}", psicologo);
+
+        return psicologoExistente;
+    }
+
+    public Page<Psicologo> listar(Pageable pageable) {
+
+        log.info("Listando todos psicólogos.");
+
+        Page<Psicologo> psicologos = gateway.listar(pageable);
+
+        log.info("Listado todos os psicólogos. Psicólogos: {}", psicologos);
+
+        return psicologos;
+    }
+
+    public Psicologo alterar(Psicologo novosDados, UUID id) {
+
+        log.info("Alterando dados do psicólogo. Novos dados: {}", novosDados);
+        log.info("\nId: {}", id);
+
+        Psicologo psicologoExistente = this.consultarPorId(id);
+
+        /*
+            Salvar novo endereço.
+         */
+
+        psicologoExistente.alterarDados(novosDados);
+
+        Psicologo psicologoSalvo = gateway.salvar(psicologoExistente);
+
+        log.info("Alterado psicólogo com sucesso. Psicólogo alterado: {}", psicologoSalvo);
+
+        return psicologoSalvo;
     }
 
 }
---
+++ src/test/java/com/liratech/helppsico/application/usecases/PsicologoUseCaseTest.java
@@ -0,0 +1,37 @@
+package com.liratech.helppsico.application.usecases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PsicologoUseCaseTest {
+
+    @Test
+    void cadastrar() {
+
+    }
+
+    @Test
+    void consultarPorId() {
+    }
+
+    @Test
+    void consultarPorNome() {
+    }
+
+    @Test
+    void consultarMelhoresAvaliados() {
+    }
+
+    @Test
+    void consultarPorCrp() {
+    }
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void alterar() {
+    }
+}
