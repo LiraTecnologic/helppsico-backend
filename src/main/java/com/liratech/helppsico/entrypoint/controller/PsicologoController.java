52474f5081ee5050b12be52a57c35e560423c17c
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-19 14:26:04 -0300

    #BTR16:feat/Adicionando os retornos dos métodos

--- src/main/java/com/liratech/helppsico/entrypoint/controller/PsicologoController.java
+++ src/main/java/com/liratech/helppsico/entrypoint/controller/PsicologoController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,7 +24,15 @@ public class PsicologoController {
 
     @PostMapping
     public ResponseEntity<ResponseDto<PsicologoDto>> cadastrar (@RequestBody PsicologoDto psicologo){
-
+        return ResponseEntity
+                .created(
+                        UriComponentsBuilder
+                                .newInstance()
+                                .path("/psicologos/{id}")
+                                .buildAndExpand()
+                                .toUri()
+                )
+                .body();
     }
 
     @GetMapping
@@ -32,16 +41,20 @@ public class PsicologoController {
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "nome,asc") String sort){
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.asc(sort.split(",")[0])));
+
+        return ResponseEntity.ok();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDto<PsicologoDto>> consultarPorId (@PathVariable UUID id){
 
+        return ResponseEntity.ok();
     }
 
     @GetMapping("/nome")
     public ResponseEntity<ResponseDto<List<PsicologoDto>>> consultarPorNome (@RequestParam String nome){
 
+        return ResponseEntity.ok();
     }
 
     @GetMapping("/melhores-avaliados")
@@ -51,14 +64,19 @@ public class PsicologoController {
             @RequestParam(defaultValue = "nome,asc") String sort){
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.asc(sort.split(",")[0])));
 
+        return ResponseEntity.ok();
     }
 
     @GetMapping("/crp")
-    public ResponseEntity<ResponseDto<PsicologoDto>> consultarPorCrp (@RequestParam String crp){}
+    public ResponseEntity<ResponseDto<PsicologoDto>> consultarPorCrp (@RequestParam String crp){
+
+        return ResponseEntity.ok();
+    }
 
     @PutMapping("/{id}")
     public ResponseEntity<ResponseDto<PsicologoDto>> alterar (@RequestBody PsicologoDto, @PathVariable UUID id){
 
+        return ResponseEntity.ok();
     }
 
 }
