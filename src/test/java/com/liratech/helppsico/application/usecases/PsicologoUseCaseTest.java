646d405fd981ebeb99167ad9df1e3d54861a416b
VitorVieira <vitorhvvieira@gmail.com>
2025-03-18 21:59:36 -0300

    #BTR17:test/Começando os testes do PsicologoUseCase

--- src/test/java/com/liratech/helppsico/application/usecases/PsicologoUseCaseTest.java
+++ src/test/java/com/liratech/helppsico/application/usecases/PsicologoUseCaseTest.java
@@ -1,14 +1,40 @@
 package com.liratech.helppsico.application.usecases;
 
+import com.liratech.helppsico.builders.PsicologoBuilder;
+import com.liratech.helppsico.domain.Psicologo;
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PsicologoUseCaseTest {
 
+    @Mock
+    private PsicologoGateway gateway;
+
+    @Mock
+    private FotoUseCase fotoUseCase;
+
+    @Captor
+    ArgumentCaptor<Psicologo> captor;
+
+    @InjectMocks
+    private PsicologoUseCase useCase;
+
     @Test
     void cadastrar() {
+        Psicologo psicologoNovo = PsicologoBuilder.gerarPsicologo();
 
+        Mockito.when(gateway.consultarPorCrp(psicologoNovo.getCrp())).thenReturn(Optional.empty());
+        Mockito.when(gateway.salvar(psicologoNovo)).thenReturn(psicologoNovo);
+
+        Mockito.when(fotoUseCase.salvarImagem(psicologoNovo.getFoto())).thenReturn("urltestefoto");
+
+        Psicologo psicologoCadastrado = useCase.cadastrar(psicologoNovo);
+
+        PsicologoValidator.validaPsicologo(psicologoCadastrado);
     }
 
     @Test
