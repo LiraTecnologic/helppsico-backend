ec94f9cff58015415f87dd342b4241d49155c5a1
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-20 19:19:52 -0300

    #BTR18:feat/Adicionando os métodos do PsicologoGateway

---
+++ src/main/java/com/liratech/helppsico/application/gateways/PsicologoGateway.java
@@ -0,0 +1,22 @@
+package com.liratech.helppsico.application.gateways;
+
+import com.liratech.helppsico.domain.Psicologo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PsicologoGateway {
+        Psicologo salvar(Psicologo psicologo);
+
+        Optional<Psicologo> consultarPorId(UUID id);
+
+        Optional<Psicologo> consultarPorNome(String nome);
+
+        Page<Psicologo> consultarMelhoresAvaliados(Pageable pageable);
+
+        Optional<Psicologo> consultarPorCrp(String crp);
+
+        Page<Psicologo> listar(Pageable pageable);
+}
