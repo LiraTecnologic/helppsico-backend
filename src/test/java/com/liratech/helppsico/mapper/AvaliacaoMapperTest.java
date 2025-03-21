824b4e1314c65b49cbada96b4b861e2db41c0c44
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-21 19:11:17 -0300

    #BTR07:test/Retirando anotações desnecessárias em AvaliacaoMapperTest e alterando o pomxml

--- src/test/java/com/liratech/helppsico/mapper/AvaliacaoMapperTest.java
+++ src/test/java/com/liratech/helppsico/mapper/AvaliacaoMapperTest.java
@@ -1,8 +1,6 @@
 package com.liratech.helppsico.mapper;
 
 import com.liratech.helppsico.builders.AvaliacaoDtoBuilder;
-import com.liratech.helppsico.builders.PacienteBuilder;
-import com.liratech.helppsico.builders.PsicologoBuilder;
 import com.liratech.helppsico.builders.AvaliacaoBuilder;
 import com.liratech.helppsico.domain.Avaliacao;
 import com.liratech.helppsico.entrypoint.dto.psicologo.AvaliacaoDto;
@@ -11,9 +9,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
-
-import java.util.UUID;
-
 
 class AvaliacaoMapperTest {
 
@@ -39,20 +34,6 @@ class AvaliacaoMapperTest {
     }
 
     @Test
-    @DisplayName("Caso de falha na transformação de DTO para Domain (psicologo nulo)")
-    void transformacaoDtoParaDomainFalha() {
-        AvaliacaoDto avaliacaoDto = AvaliacaoDto.builder()
-                .id(UUID.randomUUID())
-                .psicologo(null)
-                .paciente(null)
-                .nota(4.5)
-                .comentario("Bom psicologo")
-                .build();
-
-
-    }
-
-    @Test
     @DisplayName("Caso de sucesso na transformação de Domain para DTO")
     void transformacaoDomainParaDtoSucesso() {
         Avaliacao avaliacao = AvaliacaoBuilder.criarAvaliacao();
@@ -69,19 +50,5 @@ class AvaliacaoMapperTest {
 
         Assertions.assertEquals(avaliacao.getNota(), avaliacaoDto.getNota());
         Assertions.assertEquals(avaliacao.getComentario(), avaliacaoDto.getComentario());
-    }
-
-    @Test
-    @DisplayName("Caso de falha na transformação de Domain para DTO (nota negativa)")
-    void transformacaoDomainParaDtoFalha() {
-        Avaliacao avaliacao = Avaliacao.builder()
-                .id(UUID.randomUUID())
-                .psicologo(PsicologoBuilder.criarPsicologo())
-                .paciente(PacienteBuilder.criarPaciente())
-                .nota(-1.0)
-                .comentario("Comentário inválido")
-                .build();
-
-
     }
 }
