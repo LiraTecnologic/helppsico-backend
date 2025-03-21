4f331f42bfa99d17727c66c3c46dba7301bf2cac
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-20 21:46:13 -0300

    #BTR20:feat/Adicionando annotation

--- src/main/java/com/liratech/helppsico/infrastructure/repositories/entities/PsicologoRepository.java
+++ src/main/java/com/liratech/helppsico/infrastructure/repositories/entities/PsicologoRepository.java
@@ -2,10 +2,13 @@ package com.liratech.helppsico.infrastructure.repositories.entities;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.awt.print.Pageable;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PsicologoRepository extends JpaRepository<PsicologoEntity, UUID> {
 
     Optional<PsicologoEntity> findByNome(String nome);
