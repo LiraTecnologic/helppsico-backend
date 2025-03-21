bb52aa63aa7345bc1a317471bd9fc3f6a3a470e4
VitorVieira <vitorhvvieira@gmail.com>
2025-03-21 20:31:22 -0300

    #BTR14:test/Implementando testes da classe PacienteDataProvider

---
+++ src/test/java/com/liratech/helppsico/infrastructure/dataprovider/PacienteDataProviderTest.java
@@ -0,0 +1,92 @@
+package com.liratech.helppsico.infrastructure.dataprovider;
+
+import com.liratech.helppsico.builders.PacienteBuilder;
+import com.liratech.helppsico.domain.Paciente;
+import com.liratech.helppsico.infrastructure.dataprovider.exceptions.DataProviderException;
+import com.liratech.helppsico.infrastructure.repositories.entities.PacienteEntity;
+import com.liratech.helppsico.validators.PacienteValidator;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@RequiredArgsConstructor
+class PacienteDataProviderTest {
+
+    @Mock
+    private PacienteRepository repository;
+
+    @InjectMocks
+    private PacienteDataProvider dataProvider;
+
+    private final PacienteMapper mapper;
+
+
+    @Test
+    void testaSalvarPsicologo() {
+        Paciente pacienteTeste = PacienteBuilder.gerarPaciente();
+
+        Mockito.when(repository.save(Mockito.any())).thenReturn(mapper.paraEntity(pacienteTeste));
+        pacienteTeste.setId(null);
+        Paciente pacienteResult = dataProvider.salvar(pacienteTeste);
+        PacienteValidator.validaPaciente(pacienteResult, pacienteTeste);
+        Assertions.assertNotNull(pacienteResult);
+    }
+
+    @Test
+    void testaExceptionSalvarPsicologo() {
+        Mockito.when(repository.save(Mockito.any())).thenThrow(RuntimeException.class);
+        DataProviderException exception = Assertions
+                .assertThrows(DataProviderException.class, () -> dataProvider.salvar(PacienteBuilder.gerarPaciente()));
+        Assertions.assertEquals(exception.getMessage(), PacienteDataProvider.MENSAGEM_ERRO_SALVAR);
+    }
+
+    @Test
+    void testaConsultarPsicologoPorId() {
+        Paciente pacienteTeste = PacienteBuilder.gerarPaciente();
+
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.of(mapper.paraEntity(pacienteTeste)));
+        Optional<Paciente> pacienteResult = dataProvider.consultarPorId(pacienteTeste.getId());
+        pacienteResult.ifPresent(paciente -> {
+            PacienteValidator.validaPaciente(pacienteTeste, paciente);
+            Assertions.assertEquals(pacienteTeste.getId(), paciente.getId());
+        });
+    }
+
+    @Test
+    void testaExceptionConsultarPsicologoPorId() {
+        Mockito.when(repository.findById(Mockito.any())).thenThrow(RuntimeException.class);
+        DataProviderException exception = Assertions
+                .assertThrows(DataProviderException.class, () -> dataProvider.consultarPorId(PacienteBuilder.gerarPaciente().getId()));
+        Assertions.assertEquals(exception.getMessage(), PacienteDataProvider.MENSAGEM_ERRO_CONSULTAR_POR_ID);
+    }
+
+    @Test
+    void testaConsultaPsicologoPorEmail() {
+        Paciente pacienteTeste = PacienteBuilder.gerarPaciente();
+
+        Mockito.when(repository.findByEmail(Mockito.any())).thenReturn(Optional.of(mapper.paraEntity(pacienteTeste)));
+        Optional<Paciente> pacienteResult = dataProvider.consultarPorEmail(pacienteTeste.getEmail());
+        pacienteResult.ifPresent(paciente -> {
+            PacienteValidator.validaPaciente(pacienteTeste, paciente);
+            Assertions.assertEquals(pacienteTeste.getId(), paciente.getId());
+        });
+    }
+
+    @Test
+    void testaExceptionConsultarPsicologoPorEmail() {
+        Mockito.when(repository.findByEmail(Mockito.any())).thenThrow(RuntimeException.class);
+        DataProviderException exception = Assertions
+                .assertThrows(DataProviderException.class, () -> dataProvider.consultarPorEmail(PacienteBuilder.gerarPaciente().getEmail()));
+        Assertions.assertEquals(exception.getMessage(), PacienteDataProvider.MENSAGEM_ERRO_CONSULTAR_POR_EMAIL);
+    }
+}
