2b3c372f21da15f6da324b39dc6c0857346e9f64
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-21 14:20:42 -0300

    #BTR07:test/Arrumando mapeamento de teste Prontuario

--- src/main/java/com/liratech/helppsico/entrypoint/mapper/ProntuarioMapper.java
+++ src/main/java/com/liratech/helppsico/entrypoint/mapper/ProntuarioMapper.java
@@ -3,10 +3,17 @@ package com.liratech.helppsico.entrypoint.mapper;
 import com.liratech.helppsico.domain.Prontuario;
 import com.liratech.helppsico.entrypoint.dto.psicologo.ProntuarioDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProntuarioMapper {
+
+    @Mapping(target = "paciente.cpf", source = "cpf")
+    @Mapping(target = "psicologo.crp", source = "crp")
     Prontuario paraDomain (ProntuarioDto prontuarioDto);
+
+    @Mapping(target = "cpf", source = "paciente.cpf")
+    @Mapping(target = "crp", source = "psicologo.crp")
     ProntuarioDto paraDto (Prontuario prontuario);
 }
