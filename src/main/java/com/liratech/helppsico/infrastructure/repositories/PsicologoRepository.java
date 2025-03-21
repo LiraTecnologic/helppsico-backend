612a2b9bdd4ed4f4c5453f49bf41670e13f6743b
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-03-20 22:15:55 -0300

    #BTR20:fix/Ajustando de acordo com o PR

--- src/main/java/com/liratech/helppsico/infrastructure/repositories/PsicologoRepository.java
+++ src/main/java/com/liratech/helppsico/infrastructure/repositories/PsicologoRepository.java
@@ -3,6 +3,7 @@ package com.liratech.helppsico.infrastructure.repositories;
 import com.liratech.helppsico.infrastructure.repositories.entities.PsicologoEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
@@ -14,7 +15,14 @@ public interface PsicologoRepository extends JpaRepository<PsicologoEntity, UUID
 
     Optional<PsicologoEntity> findByNome(String nome);
 
-    Page<PsicologoEntity> findAllMelhoresAvaliados(Pageable pageable);
+    @Query("""
+            SELECT p
+            FROM psicologos p
+            LEFT JOIN avaliacoes a ON a.id_psicologo = p.id_psicologo
+            GROUP BY p.id_psicologo
+            ORDER BY AVG(a.nota) DESC
+            """)
+    Page<PsicologoEntity> consultarMelhoresAvaliados(Pageable pageable);
 
     Optional<PsicologoEntity> findByCrp(String crp);
 
