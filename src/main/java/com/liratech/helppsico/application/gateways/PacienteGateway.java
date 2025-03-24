06b8e898c6e4709e249caa598770147f85b4f80b
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-24 11:36:44 -0300

    #BTR13:refactor/Alterando nome salvarPaciente para salvar

--- src/main/java/com/liratech/helppsico/application/gateways/PacienteGateway.java
+++ src/main/java/com/liratech/helppsico/application/gateways/PacienteGateway.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface PacienteGateway {
-    Paciente salvarPaciente(Paciente paciente);
+    Paciente salvar(Paciente paciente);
 
     Optional<Paciente> consultarPorId(UUID id);
 
