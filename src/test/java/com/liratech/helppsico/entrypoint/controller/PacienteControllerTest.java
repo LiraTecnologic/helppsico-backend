cafc9928d78f42cce8da7640d8b264e98c7af1da
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-26 20:19:02 -0300

    #BTR11:test/Criando estrutura dos testes

---
+++ src/test/java/com/liratech/helppsico/entrypoint/controller/PacienteControllerTest.java
@@ -0,0 +1,30 @@
+package com.liratech.helppsico.entrypoint.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class PacienteControllerTest {
+
+    private final PacienteMapper mapper;
+    @Mock
+    private PacienteUseCase useCase;
+    @InjectMocks
+    private PacienteController controller;
+
+    @Test
+    @DisplayName("Teste de sucesso para cadastro de um novo Paciente.")
+    void testeCadastrarPaciente() {
+
+    }
+
+    @Test
+    @DisplayName("Teste de sucesso para consulta de Paciente por ID.")
+    void testeConsultaPacientePorId() {
+
+    }
+}
