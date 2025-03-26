276d15bbaa07de0af632b519f422ebb9e4d26123
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-03-26 15:16:13 -0300

    #BTR11:feat/Adicionando código inicial de PacienteController

---
+++ src/main/java/com/liratech/helppsico/entrypoint/controller/PacienteController.java
@@ -0,0 +1,44 @@
+package com.liratech.helppsico.entrypoint.controller;
+
+import com.liratech.helppsico.entrypoint.dto.PacienteDto;
+import com.liratech.helppsico.entrypoint.dto.ResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/pacientes")
+@AllArgsConstructor
+public class PacienteController {
+
+    private PacienteUseCase useCase;
+    private PacienteMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<ResponseDto<PacienteDto>> cadastrar(@RequestBody PacienteDto paciente){
+        PacienteDto pacienteNovo = mapper.paraDto(useCase.cadastrar(mapper.paraDain(paciente)));
+        ResponseDto<PacienteDto> retorno = new ResponseDto<>(pacienteNovo);
+
+        return ResponseEntity
+                .created(
+                        UriComponentsBuilder
+                                .newInstance()
+                                .path("/paciente/{id}")
+                                .buildAndExpand(pacienteNovo.getId())
+                                .toUri()
+                )
+                .body(retorno);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseDto<PacienteDto>> consultarPorId (@PathVariable UUID id){
+        PacienteDto pacienteDto = mapper.paraDto(useCase.consultarPorId(id));
+        ResponseDto<PacienteDto> retorno = new ResponseDto<>(pacienteDto);
+
+        return ResponseEntity.ok(retorno);
+    }
+
+}
