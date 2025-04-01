6ef03822fab1c3f6d5040018886f448e6ea9a2de
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-01 20:25:21 -0300

    #BTR16:feat/Adicionando PsicologoValidatorJson para os testes

--- src/test/java/com/liratech/helppsico/validators/json/PsicologoValidatorJson.java
+++ src/test/java/com/liratech/helppsico/validators/json/PsicologoValidatorJson.java
@@ -11,7 +11,14 @@ public class PsicologoValidatorJson{
                 .andExpect(jsonPath("$.dado.nome").value(esperado.getNome()))
                 .andExpect(jsonPath("$.dado.crp").value(esperado.getCrp()))
                 .andExpect(jsonPath("$.dado.cpf").value(esperado.getCpf()))
-
+                .andExpect(jsonPath("$.dado.email").value(esperado.getEmail()))
+                .andExpect(jsonPath("$.dado.telefone").value(esperado.getTelefone()))
+                .andExpect(jsonPath("$.dado.dataNascimento").value(esperado.getDataNascimento().toString()))
+                .andExpect(jsonPath("$.dado.senha").value(esperado.getSenha()))
+                .andExpect(jsonPath("$.dado.genero").value(esperado.getGenero().toString()))
+                .andExpect(jsonPath("$.dado.endereco").value(esperado.getEnderecoAtendimento().toString()))
+                .andExpect(jsonPath("$.dado.fotoUrl").value(esperado.getFotoUrl()))
+                .andExpect(jsonPath("$.dado.biografia").value(esperado.getBiografia()))
                 .andExpect(jsonPath("$.erro").doesNotExist());
     }
 }
