e151c7eff63559dcf23a1c82b66c21a9d428885d
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-02 20:56:49 -0300

    #BTR16:test/Finalizando teste de métodos GET

--- src/test/java/com/liratech/helppsico/entrypoint/controller/PsicologoControllerTest.java
+++ src/test/java/com/liratech/helppsico/entrypoint/controller/PsicologoControllerTest.java
@@ -3,20 +3,24 @@ package com.liratech.helppsico.entrypoint.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.liratech.helppsico.application.usecases.PsicologoUseCase;
+
 import com.liratech.helppsico.builders.PsicologoBuilder;
 import com.liratech.helppsico.domain.Psicologo;
 import com.liratech.helppsico.entrypoint.dto.psicologo.PsicologoDto;
-import com.liratech.helppsico.entrypoint.mapper.EnderecoMapper;
 import com.liratech.helppsico.entrypoint.mapper.PsicologoMapper;
+import com.liratech.helppsico.infrastructure.mapper.EnderecoMapper;
+import com.liratech.helppsico.infrastructure.repositories.EnderecoRepository;
 import com.liratech.helppsico.infrastructure.repositories.PsicologoRepository;
 import com.liratech.helppsico.infrastructure.repositories.entities.EnderecoEntity;
 import com.liratech.helppsico.infrastructure.repositories.entities.PsicologoEntity;
 import com.liratech.helppsico.validators.json.PsicologoValidatorJson;
+
 import lombok.RequiredArgsConstructor;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
@@ -27,13 +31,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import java.util.Optional;
 import java.util.UUID;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RequiredArgsConstructor
 @SpringBootTest
@@ -44,8 +46,8 @@ public class PsicologoControllerTest {
     private final MockMvc mockMvc;
     private final ObjectMapper objectMapper;
     private final PsicologoMapper mapperEntry;
-    private final com.liratech.helppsico.infrastructure.mappers.PsicologoMapper mapperInfra;
-    private final EnderecoMapper
+    private final com.liratech.helppsico.infrastructure.mapper.PsicologoMapper mapperInfra;
+    private final EnderecoMapper enderecoMapper;
 
     @MockitoSpyBean
     private final PsicologoRepository repository;
@@ -65,11 +67,11 @@ public class PsicologoControllerTest {
     @Test
     void testeCadastrarPsicologo() throws Exception {
         psicologoDtoEntrada.setId(null);
-        EnderecoEntity enderecoEntity = mapp
+        EnderecoEntity enderecoEntity = enderecoMapper.paraEntity(psicologoDomain.getEnderecoAtendimento());
 
         Mockito.when(repository.findByCrp(Mockito.any())).thenReturn(Optional.empty());
         Mockito.when(repositoryEndereco.findById(Mockito.any())).thenReturn(Optional.empty());
-        Mockito.when(repositoryEndereco.save(Mockito.any())).thenReturn();
+        Mockito.when(repositoryEndereco.save(Mockito.any())).thenReturn(enderecoEntity);
         Mockito.when(repository.save(Mockito.any())).thenReturn(mapperInfra.paraEntity(psicologoDomain));
 
 
@@ -92,7 +94,6 @@ public class PsicologoControllerTest {
         int page = 0;
         int size = 10;
         String sort = "nome,asc";
-
         Pageable pageable = PageRequest.of(page,size);
 
         Page<Psicologo> paginaDomain = PsicologoBuilder.criarPageDePsicologos();
@@ -110,9 +111,67 @@ public class PsicologoControllerTest {
     }
 
     @Test
-    void testeConsultarPorId() throws Exception{
+    void testeConsultarPorId() throws Exception {
         UUID idRequest = psicologoDtoEntrada.getId();
 
-        Mockito.when()
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.of(mapperInfra.paraEntity(psicologoDomain)));
+
+        ResultActions resultado = mockMvc.perform(get("/psicologos/{id}", idRequest))
+                .andExpect(status().isOk());
+
+        PsicologoValidatorJson.validaPsicologoJson(resultado, psicologoDtoEntrada);
+    }
+
+    @Test
+    void testeConsultarPorNome() throws Exception {
+        int page = 0;
+        int size = 10;
+        String sort = "nome,asc";
+        Pageable pageable = PageRequest.of(page,size);
+
+        Page<Psicologo> paginaDomain = PsicologoBuilder.criarPageDePsicologos();
+        Page<PsicologoEntity> paginaEntity = paginaDomain.map(mapperInfra::paraEntity);
+
+        Mockito.when(repository.findByNome(Mockito.any())).thenReturn(paginaEntity);
+
+        ResultActions resultado = mockMvc.perform(get("/psicologos/nome")
+                        .param("nome", "joão-silva"))
+                .andExpect(status().isOk());
+
+        PsicologoValidatorJson.validaPageResponse(resultado);
+    }
+
+    @Test
+    void testeConsultarMelhoresAvaliados() throws Exception {
+        int page = 0;
+        int size = 10;
+        String sort = "nome,asc";
+        Pageable pageable = PageRequest.of(page,size);
+
+        Page<Psicologo> paginaDomain = PsicologoBuilder.criarPageDePsicologos();
+        Page<PsicologoEntity> paginaEntity = paginaDomain.map(mapperInfra::paraEntity);
+
+        Mockito.when(repository.consultarMelhoresAvaliados(Mockito.any())).thenReturn(paginaEntity);
+
+        ResultActions resultado = mockMvc.perform(get("/psicologos/melhores-avaliados")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size))
+                        .param("sort", sort))
+                .andExpect(status().isOk());
+
+        PsicologoValidatorJson.validaPageResponse(resultado);
+    }
+
+    @Test
+    void testeConsultarPorCrp() throws Exception {
+        String crpParam = psicologoDtoEntrada.getCrp();
+
+        Mockito.when(repository.findById(Mockito.any())).thenReturn(Optional.of(mapperInfra.paraEntity(psicologoDomain)));
+
+        ResultActions resultado = mockMvc.perform(get("/psicologos/crp")
+                        .param("crp", crpParam))
+                .andExpect(status().isOk());
+
+        PsicologoValidatorJson.validaPsicologoJson(resultado, psicologoDtoEntrada);
     }
 }
