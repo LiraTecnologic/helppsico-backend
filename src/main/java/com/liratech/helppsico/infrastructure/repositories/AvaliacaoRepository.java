4d23d9d71fe1f930bfc9ff9bcd291ea7fe5df8f8
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-03 12:28:21 -0300

    #BTR25:feat/Implementando AvaliacaoRepository

---
+++ src/main/java/com/liratech/helppsico/infrastructure/repositories/AvaliacaoRepository.java
@@ -0,0 +1,13 @@
+package com.liratech.helppsico.infrastructure.repositories;
+
+import com.liratech.helppsico.infrastructure.repositories.entities.AvaliacaoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface AvaliacaoRepository extends JpaRepository<AvaliacaoEntity, UUID> {
+    List<AvaliacaoEntity> listarPorPsicologos(UUID id);
+}
