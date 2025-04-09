2fd95ceb58d8ed1615f115ec19c4fc78d2cd0ba2
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-04-09 17:00:51 -0300

    #BTR12/test: Adicionando o código de PacienteUseCase seguindo exemplo de PsicologoUseCase

---
+++ src/test/java/com/liratech/helppsico/application/usecases/PacienteUseCaseTest.java
@@ -0,0 +1,74 @@
+package com.liratech.helppsico.application.usecases;
+
+import com.liratech.helppsico.application.exceptions.PacienteExistenteException;
+import com.liratech.helppsico.application.exceptions.PacienteNaoEncontradoException;
+import com.liratech.helppsico.application.gateways.PacienteGateway;
+import com.liratech.helppsico.builders.PacienteBuilder;
+import com.liratech.helppsico.domain.Paciente;
+import com.liratech.helppsico.validators.PacienteValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+class PacienteUseCaseTest {
+
+    @Mock
+    private PacienteGateway gateway;
+
+    @Mock
+    private FotoUseCase fotoUseCase;
+
+    @Mock
+    private EnderecoUseCase enderecoUseCase;
+
+    @InjectMocks
+    private PacienteUseCase useCase;
+
+    @Test
+    void testeCadastrarPaciente() {
+        Paciente novoPaciente = PacienteBuilder.criarPaciente();
+
+        Mockito.when(gateway.consultarPorEmail(Mockito.any())).thenReturn(Optional.empty());
+        Mockito.when(gateway.salvar(Mockito.any())).thenReturn(novoPaciente);
+        Mockito.when(fotoUseCase.salvarImagem(Mockito.any())).thenReturn(novoPaciente.getFotoUrl());
+        Mockito.when(enderecoUseCase.cadastrar(Mockito.any())).thenReturn(novoPaciente.getEndereco());
+
+        Paciente pacienteCadastrado = useCase.cadastrar(novoPaciente);
+        PacienteValidator.validaPacienteDomain(novoPaciente, pacienteCadastrado);
+    }
+
+    @Test
+    void testeErroCadastrarPaciente() {
+        Paciente novoPaciente = PacienteBuilder.criarPaciente();
+
+        Mockito.when(gateway.consultarPorEmail(Mockito.any())).thenReturn(Optional.of(PacienteBuilder.criarPaciente()));
+        PacienteExistenteException exception = Assertions.assertThrows(PacienteExistenteException.class, () -> useCase.cadastrar(novoPaciente));
+        Assertions.assertEquals(MENSAGEM_PACIENTE_JA_EXISTE, exception.getMessage());
+        Mockito.verify(gateway, Mockito.times(1)).consultarPorEmail(novoPaciente.getEmail());
+    }
+
+    @Test
+    void testeConsultarPacientePorId() {
+        Paciente pacienteCriado = PacienteBuilder.criarPaciente();
+
+        Mockito.when(gateway.consultarPorId(Mockito.any())).thenReturn(Optional.of(pacienteCriado));
+        Paciente paciente = useCase.consultarPorId(pacienteCriado.getId());
+        PacienteValidator.validaPacienteDomain(pacienteCriado, paciente);
+    }
+
+    @Test
+    void testeErroConsultaPacientePorId() {
+        UUID id = PacienteBuilder.criarPaciente().getId();
+
+        Mockito.when(gateway.consultarPorId(Mockito.any())).thenReturn(Optional.empty());
+        PacienteNaoEncontradoException exception = Assertions.assertThrows(PacienteNaoEncontradoException.class, () -> useCase.consultarPorId(id));
+        Assertions.assertEquals(MENSAGEM_PACIENTE_NAO_ENCONTRADO, exception.getMessage());
+        Mockito.verify(gateway, Mockito.times(1)).consultarPorId(id);
+    }
+}
