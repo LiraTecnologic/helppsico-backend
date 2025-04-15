bc5f85c38895eec526f5de7742eec803ff1b1419
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-04-15 20:27:32 -0300

    #BTR72/feat: Adicionando método Listar

--- src/main/java/com/liratech/helppsico/application/gateways/ValidacaoCrpGateway.java
+++ src/main/java/com/liratech/helppsico/application/gateways/ValidacaoCrpGateway.java
@@ -1,5 +1,7 @@
 package com.liratech.helppsico.application.gateways;
 
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,6 +9,8 @@ public interface ValidacaoCrpGateway {
     ValidacaoCrp salvar(ValidacaoCrp validacaoCrp);
 
     Optional<ValidacaoCrp> consultarPorId(UUID id);
+
+    Page<ValidacaoCrp> listar(Pageable pageable);
 
     void deletar(UUID id);
 }
