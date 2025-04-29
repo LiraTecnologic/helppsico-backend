f7ef556151df8fc161e3d6f616f27b6231a4dc4c
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-29 19:01:02 -0300

    #BTR33:feat/Finalizando o logarPaciente

--- src/main/java/com/liratech/helppsico/application/usecases/LoginUseCase.java
+++ src/main/java/com/liratech/helppsico/application/usecases/LoginUseCase.java
@@ -18,9 +18,10 @@ public class LoginUseCase {
     private final CriptografiaUseCase criptografiaUseCase;
 
     public static final String MENSAGEM_SENHA_INVALIDA = "Senha do usuario inválida.";
+    public static final String MENSAGEM_TIPO_INVALIDO = "Tipo do token errado!";
 
-    public LoginRespostaDto logarPaciente(String email, String senha, String token){
-        log.info("Iniciando processo de login do paciente. Email:{}, Senha:{}", email, senha);
+    public LoginRespostaDto logarPaciente(String email, String senha){
+        log.info("Iniciando processo de login do paciente. Email:{}", email);
 
         Paciente pacienteBuscado = pacienteUseCase.consultarPorEmail(email);
 
@@ -28,9 +29,16 @@ public class LoginUseCase {
             throw new SenhaInvalidaException(MENSAGEM_SENHA_INVALIDA);
         }
 
-        autenticacaoUseCase.validarToken(token);
+        String token = autenticacaoUseCase.gerarTokenPaciente(pacienteBuscado);
 
-        return
+        log.info("Processo de login finalizado.");
+
+        return LoginRespostaDto.builder()
+                .idPaciente(pacienteBuscado.getId())
+                .email(email)
+                .senha(senha)
+                .token(token)
+                .build();
     }
 
     public LoginDto logarPsicologo(String idPsicologo, String token){
--- src/main/java/com/liratech/helppsico/entrypoint/dto/LoginRespostaDto.java
+++ src/main/java/com/liratech/helppsico/entrypoint/dto/LoginRespostaDto.java
@@ -1,4 +1,17 @@
 package com.liratech.helppsico.entrypoint.dto;
 
+import lombok.*;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
 public class LoginRespostaDto {
+    private UUID idPaciente;
+    private String email;
+    private String senha;
+    private String token;
 }
