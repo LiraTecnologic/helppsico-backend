e5b1463cf8695925c4eeb46a08a1966ecb69f9d0
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-05-03 11:23:38 -0300

    #BTR80:chore/Ajustando correções do PR

--- src/main/java/com/liratech/helppsico/domain/Foto.java
+++ src/main/java/com/liratech/helppsico/domain/Foto.java
@@ -1,9 +1,5 @@
 package com.liratech.helppsico.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.liratech.helppsico.entrypoint.dto.PacienteDto;
-import com.liratech.helppsico.entrypoint.dto.psicologo.PsicologoDto;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,7 +13,7 @@ import java.util.UUID;
 @Builder
 public class Foto {
     private UUID id;
-    private PsicologoDto psicologo;
-    private PacienteDto paciente;
+    private Psicologo psicologo;
+    private Paciente paciente;
     private String fotoUrl;
 }
