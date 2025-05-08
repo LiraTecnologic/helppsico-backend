9ec2254f8981b2c9d7b0cccae256533d412829a7
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-05-08 00:07:48 -0300

    #BTR71:feat/Adicionando primeira parte do código do UseCase

---
+++ src/main/java/com/liratech/helppsico/application/exceptions/validacaoCrp/ValidacaoCrpExistenteException.java
@@ -0,0 +1,7 @@
+package com.liratech.helppsico.application.exceptions.validacaoCrp;
+
+public class ValidacaoCrpExistenteException extends RuntimeException {
+    public ValidacaoCrpExistenteException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/liratech/helppsico/application/usecases/ValidacaoCrpUseCase.java
@@ -0,0 +1,51 @@
+package com.liratech.helppsico.application.usecases;
+
+import com.liratech.helppsico.application.exceptions.validacaoCrp.ValidacaoCrpExistenteException;
+import com.liratech.helppsico.application.gateways.ValidacaoCrpGateway;
+import com.liratech.helppsico.domain.Psicologo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ValidacaoCrpUseCase {
+
+    private final ValidacaoCrpGateway gateway;
+    private final PsicologoUseCase psicologoUseCase;
+
+    public static final String MENSAGEM_VALIDACAO_CRP_JA_CADASTRADA = "Validação CRP já está cadastrada";
+
+    public ValidacaoCrp criar(ValidacaoCrp validacaoCrp){
+        log.info("Criando Validação de CRP. Nova Validação: {}", validacaoCrp);
+
+        Psicologo psicologo = psicologoUseCase.consultarPorCrp(validacaoCrp.getCrp());
+        validacaoCrp.setPsicologo(psicologo);
+
+        Optional<ValidacaoCrp> validacaoConsulta =  ;
+
+        if(validacaoConsulta.isPresent()){
+            throw new ValidacaoCrpExistenteException(MENSAGEM_VALIDACAO_CRP_JA_CADASTRADA);
+        }
+
+        ValicadacaoCrp validacaoSalva = gateway.salvar(validacaoCrp);
+
+        log.info("Validação criada com sucesso. Validação: {}", validacaoSalva);
+
+        return validacaoSalva;
+    }
+
+    public ValidacaoCrp validar(ValidacaoCrp validacaoCrp, UUID id){
+
+    }
+
+    public Page<ValidacaoCrp> listar(Pageable pageable){
+
+    }
+}
