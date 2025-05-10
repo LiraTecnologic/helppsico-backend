42612fb93788edd0b9991694a233a27eeeb0241d
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-05-10 07:58:14 -0300

    #BTR44:feat/Implementando código (intensivão)

---
+++ src/test/java/com/liratech/helppsico/infrastructure/dataprovider/DocumentoDataProviderTest.java
@@ -0,0 +1,101 @@
+package com.liratech.helppsico.infrastructure.dataprovider;
+
+import com.liratech.helppsico.builders.DocumentoBuilder;
+import com.liratech.helppsico.domain.documento.Documento;
+import com.liratech.helppsico.infrastructure.dataprovider.exceptions.DataProviderException;
+
+import com.liratech.helppsico.infrastructure.repositories.entities.documento.DocumentoEntity;
+import com.liratech.helppsico.validators.DocumentoValidator;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static com.liratech.helppsico.infrastructure.dataprovider.DocumentoDataProvider.MENSAGEM_ERRO_LISTAR;
+import static com.liratech.helppsico.infrastructure.dataprovider.DocumentoDataProvider.MENSAGEM_ERRO_SALVAR;
+
+
+@ExtendWith(MockitoExtension.class)
+@RequiredArgsConstructor
+class DocumentoDataProviderTest {
+
+    @Mock
+    private final DocumentoRepository repository;
+
+    @InjectMocks
+    private final DocumentoDataProvider dataProvider;
+
+    private Documento documentoDomain;
+    private DocumentoEntity documentoEntity;
+    private DocumentoMapper mapper;
+    private Page<Documento> pageDocumentos;
+    private Page<DocumentoEntity> pageDocumentosEntity;
+    private Pageable pageable;
+
+    @BeforeEach
+    void inicializarAtributos(){
+        documentoDomain = DocumentoBuilder.criarAtestado();
+        documentoEntity = DocumentoBuilder.criarAtestadoEntity();
+        pageDocumentos = DocumentoBuilder.criarPageDeDocumento();
+        pageDocumentosEntity = pageDocumentos.map(mapper::paraEntity);
+
+        pageable = PageRequest.of(0,10);
+    }
+
+    @Test
+    void testeSalvarDocumento(){
+        documentoDomain.setId(null);
+
+        Mockito.when(repository.save(Mockito.any())).thenReturn(documentoEntity);
+
+        Documento documento = dataProvider.salvar(documentoDomain);
+
+        Assertions.assertNotNull(documento.getId());
+        DocumentoValidator.validaDocumentoDomain(mapper.paraDomain(documentoEntity), documento);
+    }
+
+    @Test
+    void testeErroSalvarDocumento(){
+        Mockito.when(repository.save(Mockito.any())).thenThrow(Exception.class);
+
+        DataProviderException exception = Assertions.assertThrows(
+                DataProviderException.class,
+                () -> dataProvider.salvar(documentoDomain)
+        );
+
+        Assertions.assertEquals(MENSAGEM_ERRO_SALVAR, exception.getMessage());
+    }
+
+    @Test
+    void testeListarDocumentos(){
+        Mockito.when(repository.findAll(Mockito.any())).thenReturn(pageDocumentosEntity);
+
+        Page<Documento> documentos = dataProvider.listar(pageable);
+
+        documentos.map(documento -> {
+            Assertions.assertNotNull(documento.getId());
+            DocumentoValidator.validaDocumentoDomain(documentoDomain, documento);
+            return null;
+        });
+    }
+
+    @Test
+    void testeErroListarDocumentos(){
+        Mockito.when(repository.findAll(Mockito.any())).thenThrow(Exception.class);
+
+        DataProviderException exception = Assertions.assertThrows(
+                DataProviderException.class,
+                () -> dataProvider.listar(pageable)
+        );
+
+        Assertions.assertEquals(MENSAGEM_ERRO_LISTAR, exception.getMessage());
+    }
+}
