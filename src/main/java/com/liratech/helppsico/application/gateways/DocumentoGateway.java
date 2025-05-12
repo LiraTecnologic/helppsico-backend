57cbbc5ceb29f8aecbafa025fc66280abab7658c
André Gustavo <andregustavodeoliveira49@gmail.com>
2025-05-12 18:51:24 -0300

    #BTR43:feat/Arrumando detalhe que esqueci do gateway

--- src/main/java/com/liratech/helppsico/application/gateways/DocumentoGateway.java
+++ src/main/java/com/liratech/helppsico/application/gateways/DocumentoGateway.java
@@ -2,9 +2,10 @@ package com.liratech.helppsico.application.gateways;
 
 import com.liratech.helppsico.domain.Vinculo;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface DocumentoGateway {
     Vinculo salvar(Vinculo vinculo);
 
-    Page<Vinculo> listar();
+    Page<Vinculo> listar(Pageable pageable);
 }
