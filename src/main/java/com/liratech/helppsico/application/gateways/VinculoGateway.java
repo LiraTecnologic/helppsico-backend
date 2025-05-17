90f6afb6d414f4523d19c849cc85924dc0aef21a
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-05-16 23:37:56 -0300

    #BTR48:fix/Adicionando pageable

--- src/main/java/com/liratech/helppsico/application/gateways/VinculoGateway.java
+++ src/main/java/com/liratech/helppsico/application/gateways/VinculoGateway.java
@@ -2,6 +2,7 @@ package com.liratech.helppsico.application.gateways;
 
 import com.liratech.helppsico.domain.Vinculo;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,6 +11,6 @@ public interface VinculoGateway {
     Vinculo salvar(Vinculo vinculo);
     Optional<Vinculo> consultarPorId(UUID id);
     void deletar(UUID id);
-    Page<Vinculo> listarPorIdPsicologo(UUID idPsicologo);
+    Page<Vinculo> listarPorIdPsicologo(UUID idPsicologo, Pageable pageable);
     Optional<Vinculo> consultarPorIdPaciente(UUID idPaciente);
 }
