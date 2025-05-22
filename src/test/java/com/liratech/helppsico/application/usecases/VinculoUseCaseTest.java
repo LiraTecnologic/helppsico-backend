cca35ab475cb307d845f2c0ba86d2cfce73db87c
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-05-22 19:13:38 -0300

    #BTR47:test/Finalizando os testes

--- src/test/java/com/liratech/helppsico/application/usecases/VinculoUseCaseTest.java
+++ src/test/java/com/liratech/helppsico/application/usecases/VinculoUseCaseTest.java
@@ -15,6 +15,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import static com.liratech.helppsico.application.usecases.VinculoUseCase.ERRO_VINCULO_INVALIDO;
 
@@ -38,6 +43,8 @@ public class VinculoUseCaseTest {
     private ArgumentCaptor<Vinculo> captor;
 
     private Vinculo vinculoTeste;
+    private Vinculo vinculoAtivoTeste;
+    private Page<Vinculo> vinculoPageTeste;
     private Psicologo psicologoTeste;
     private Paciente pacienteTeste;
 
@@ -46,10 +53,17 @@ public class VinculoUseCaseTest {
         vinculoTeste = VinculoBuilder.criarVinculo();
         pacienteTeste = vinculoTeste.getPaciente();
         psicologoTeste = vinculoTeste.getPsicologo();
+
+        vinculoAtivoTeste = vinculoTeste;
+        vinculoAtivoTeste.setStatus(StatusVinculo.ATIVO);
+
+        vinculoPageTeste = VinculoBuilder.criarPageDeVinculos();
     }
 
     @Test
     void testeCriarSolicitacaoVinculo(){
+        vinculoTeste.setId(null);
+
         Mockito.when(pacienteUseCase.consultarPorId(Mockito.any())).thenReturn(pacienteTeste);
         Mockito.when(psicologoUseCase.consultarPorId(Mockito.any())).thenReturn(psicologoTeste);
         Mockito.when(gateway.salvar(captor.capture())).thenReturn(vinculoTeste);
@@ -78,8 +92,46 @@ public class VinculoUseCaseTest {
         Assertions.assertEquals(ERRO_VINCULO_INVALIDO, exception.getMessage());
     }
 
+    @Test
     void testeAceitarSolicitacao(){
+        Mockito.when(gateway.consultarPorId(Mockito.any())).thenReturn(Optional.of(vinculoTeste));
+        Mockito.when(gateway.salvar(captor.capture())).thenReturn(vinculoAtivoTeste);
 
+        useCase.aceitarSolicitacao(vinculoTeste.getId());
+        Vinculo vinculoSalvo = captor.getValue();
+
+        Assertions.assertNotNull(vinculoSalvo.getId());
+        VinculoValidator.validaVinculoDomain(vinculoAtivoTeste, vinculoSalvo);
     }
 
+    @Test
+    void testeDesvincular(){
+        Mockito.when(gateway.consultarPorId(Mockito.any())).thenReturn(Optional.of(vinculoTeste));
+        Mockito.doNothing().when(gateway).deletar(Mockito.any());
+
+        useCase.desvincular(vinculoTeste.getId());
+        Mockito.verify(gateway).deletar(Mockito.any());
+    }
+
+    @Test
+    void testeListarSolicitacoesPorIdPsicologo(){
+        Mockito.when(gateway.listarPorIdPsicologo(Mockito.any(), Mockito.any())).thenReturn(vinculoPageTeste);
+
+        Page<Vinculo> vinculoPageResultado = useCase.listarPorIdPsicologo(psicologoTeste.getId(), PageRequest.of(0, 10));
+
+        vinculoPageResultado.forEach(vinculo -> {
+            Assertions.assertNotNull(vinculo.getId());
+            VinculoValidator.validaVinculoDomain(vinculoTeste, vinculo);
+        });
+    }
+
+    @Test
+    void testeConsultarVinculoPorId(){
+        Mockito.when(gateway.consultarPorIdPaciente(Mockito.any())).thenReturn(Optional.of(vinculoTeste));
+
+        Vinculo vinculoResultado = useCase.consultarPorIdPaciente(pacienteTeste.getId());
+
+        Assertions.assertNotNull(vinculoResultado.getId());
+        VinculoValidator.validaVinculoDomain(vinculoTeste, vinculoResultado);
+    }
 }
